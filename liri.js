306b7e81b73f188b33b165ec755eb3ed0ef9f1c0
sweedishhfish04 <godziwa4@yahoo.com>
2018-07-16 18:30:27 -0500

    "getting spotify figured out"

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var twitter = require('twitter')
 var keys = require('./keys')
 var request = require('request')
 var fs = require('fs')
-var spotify = require('node-spotify-api')
+var Spotify = require('node-spotify-api')
 
 // Get rid of first two args
 process.argv.shift()
@@ -37,65 +37,60 @@ function runCommand(command, argument) {
     } else if (command === `spotify-this-song`) {
         var song = argument || 'The Sign'
 
-        var search = {
-            url: spotify + song
+        var spotify = new Spotify(keys.spotify);
+
+        spotify
+            .request('https://api.spotify.com/v1/search?type=track&limit=1&q=' + song)
+            .then(function (data) {
+                console.log(JSON.stringify(data));
+            })
+            .catch(function (error) {
+                console.error('Error occurred: ' + error);
+            })
+
+
+    } else if (command === `movie-this`) {
+
+        var movie = argument || 'Mr. Nobody,'
+
+        var options = {
+            url: keys.omdb + movie
         }
         request(options, (error, response, body) => {
-                if (error) {
-                    console.error(error.message)
-                } else {
-                    body = JSON.parse(body)
-                    var info = {
-                        artists: 
-                        Name:
-                        link:
-                        album:
-                    }
+            if (error) {
+                console.error(error.message)
+            } else {
+                body = JSON.parse(body)
+                var details = {
+                    Title: body.Title,
+                    Year: body.Year,
+                    Ratings: {
+                        IMDB: body.Ratings.find(rating => rating.Source === 'Internet Movie Database').Value,
+                        Rotten_Tomatoes: body.Ratings.find(rating => rating.Source === 'Rotten Tomatoes').Value
+                    },
+                    Country: body.Country,
+                    Language: body.Language,
+                    Plot: body.Plot,
+                    Actors: body.Actors
                 }
-
-
-            } else if (command === `movie-this`) {
-
-                var movie = argument || 'Mr. Nobody,'
-
-                var options = {
-                    url: keys.omdb + movie
-                }
-                request(options, (error, response, body) => {
-                    if (error) {
-                        console.error(error.message)
-                    } else {
-                        body = JSON.parse(body)
-                        var details = {
-                            Title: body.Title,
-                            Year: body.Year,
-                            Ratings: {
-                                IMDB: body.Ratings.find(rating => rating.Source === 'Internet Movie Database').Value,
-                                Rotten_Tomatoes: body.Ratings.find(rating => rating.Source === 'Rotten Tomatoes').Value
-                            },
-                            Country: body.Country,
-                            Language: body.Language,
-                            Plot: body.Plot,
-                            Actors: body.Actors
-                        }
-                        console.log(details)
-                    }
-                })
-            } else if (command === `do-what-it-says`) {
-                fs.readFile('random.txt', {
-                    encoding: 'utf-8'
-                }, function (error, data) {
-                    if (error) {
-                        console.error(error.message)
-                    } else {
-                        var args = data.split(',')
-
-                        // Get command and argument
-                        var command = args.shift()
-                        var argument = args.shift()
-
-                        runCommand(command, argument)
-                    }
-                })
+                console.log(details)
             }
-        }
+        })
+    } else if (command === `do-what-it-says`) {
+        fs.readFile('random.txt', {
+            encoding: 'utf-8'
+        }, function (error, data) {
+            if (error) {
+                console.error(error.message)
+            } else {
+                var args = data.split(',')
+
+                // Get command and argument
+                var command = args.shift()
+                var argument = args.shift()
+
+                runCommand(command, argument)
+            }
+        })
+    }
+}
